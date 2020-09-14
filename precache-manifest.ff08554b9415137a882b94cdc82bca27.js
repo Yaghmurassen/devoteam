66f9aaf7ff02815907de23ede3aa4fdd7056f6da
Yaghmurassen Sainson <yaghmurassen.sainson@autovisual.com>
2020-09-14 16:15:33 +0200

    Updates

---
+++ precache-manifest.ff08554b9415137a882b94cdc82bca27.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0490b6bb3055d684169fc4e4429312e",
+    "url": "/devoteam/index.html"
+  },
+  {
+    "revision": "7950b16268ea43c1e648",
+    "url": "/devoteam/static/css/main.71cff088.chunk.css"
+  },
+  {
+    "revision": "9ac40f5140ce8fe2d775",
+    "url": "/devoteam/static/js/2.f579c5b0.chunk.js"
+  },
+  {
+    "revision": "743b377795b48e0bfd5389f04b4c95aa",
+    "url": "/devoteam/static/js/2.f579c5b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7950b16268ea43c1e648",
+    "url": "/devoteam/static/js/main.a6475c4e.chunk.js"
+  },
+  {
+    "revision": "db4155722ff7c5d64f82",
+    "url": "/devoteam/static/js/runtime-main.d92815fe.js"
+  },
+  {
+    "revision": "ae213666891ff1670debb984b0555d2f",
+    "url": "/devoteam/static/media/arrow-right.ae213666.svg"
+  },
+  {
+    "revision": "17efa687d8746dbe51c6046199c62ff3",
+    "url": "/devoteam/static/media/calendar.17efa687.svg"
+  },
+  {
+    "revision": "4158d7436a0d9f3d14bc4c7ecaeb0c37",
+    "url": "/devoteam/static/media/comment.4158d743.svg"
+  },
+  {
+    "revision": "e41d1aadfdcf9b9090e830515b65fca1",
+    "url": "/devoteam/static/media/computer.e41d1aad.svg"
+  },
+  {
+    "revision": "7b0dc7ee756a67b003dafecc0694f92d",
+    "url": "/devoteam/static/media/edit.7b0dc7ee.svg"
+  },
+  {
+    "revision": "2c247094ecf0f60002a0ff4c3f4589e9",
+    "url": "/devoteam/static/media/folder.2c247094.svg"
+  },
+  {
+    "revision": "237feddbbeef883fb7ffeeb1fffb39ea",
+    "url": "/devoteam/static/media/icon-plus.237feddb.svg"
+  },
+  {
+    "revision": "59a47dfb817f3364312ef57df475cb62",
+    "url": "/devoteam/static/media/search-icon.59a47dfb.svg"
+  },
+  {
+    "revision": "396111d372de7694664b90a41493781a",
+    "url": "/devoteam/static/media/upgradepro.396111d3.png"
+  },
+  {
+    "revision": "c7215286740ce79ab7dbde4e1e506215",
+    "url": "/devoteam/static/media/user-profile.c7215286.svg"
+  }
+]);
